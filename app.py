3faefd5ff1ad4019e782742bd5e12e99bc20c88c
Campbell Forsyth <camsh69@gmail.com>
2021-12-10 15:46:37 +0000

    created manufacturers controller and modified app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 
+from controllers.manufacturers_controller import manufacturers_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(manufacturers_blueprint)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- console.py
+++ console.py
@@ -7,13 +7,13 @@ manufacturer_repository.delete_all()
 manufacturer1 = Manufacturer('Fender', True)
 manufacturer_repository.save(manufacturer1)
 
-manufacturer2 = Manufacturer('Gibson', False)
+manufacturer2 = Manufacturer('Gibson', True)
 manufacturer_repository.save(manufacturer2)
 
 # print(manufacturer1.name, manufacturer1.is_active, manufacturer1.id)
 
 # print(manufacturer2.name, manufacturer2.is_active, manufacturer2.id)
 
-names = manufacturer_repository.select_all()
-for name in names:
-    print(name.name)
+manufacturers = manufacturer_repository.select_all()
+for manufacturer in manufacturers:
+    print(manufacturer.name, manufacturer.is_active, manufacturer.id)
