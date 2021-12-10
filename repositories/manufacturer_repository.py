9cf318498f7cd9fdb76aecbcac911e61bdade189
Campbell Forsyth <camsh69@gmail.com>
2021-12-10 14:28:41 +0000

    added save function for manufacturer

--- repositories/manufacturer_repository.py
+++ repositories/manufacturer_repository.py
@@ -6,3 +6,12 @@ from models.manufacturer import Manufacturer
 def delete_all():
     sql = "DELETE * FROM manufacturers"
     run_sql(sql)
+
+
+def save(manufacturer):
+    sql = "INSERT INTO manufacturers (name, active) VALUES (%s, %s) RETURNING *"
+    values = [manufacturer.name, manufacturer.active]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    manufacturer.id = id
+    return manufacturer
