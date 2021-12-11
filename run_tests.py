844ac1f1ec43bdc2e86031cf109d84ef66215073
Campbell Forsyth <camsh69@gmail.com>
2021-12-11 14:18:20 +0000

    added unit testing files

---
+++ run_tests.py
@@ -0,0 +1,5 @@
+import unittest
+from tests.product_test import TestProduct
+
+if __name__ == ' __main__':
+    unittest.main()
---
+++ tests/product_test.py
@@ -0,0 +1,8 @@
+import unittest
+from models.product import Product
+
+
+class TestProduct(unittest.TestCase):
+
+    def setUp(self):
+        pass
