7ac6c42a6dada46b2c11ae73eb14cb3858894c11
Campbell Forsyth <camsh69@gmail.com>
2021-12-11 12:39:41 +0000

    added save function to product repository

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -7,3 +7,13 @@ from models.manufacturer import Manufacturer
 def delete_all():
     sql = "DELETE FROM products"
     run_sql(sql)
+
+
+def save(product):
+    sql = "INSERT INTO products (item, description, category, stock_quantity, buying_cost, selling_price, manufacturer_id) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [product.item, product.description, product.category, product.stock_quantity,
+              product.buying.cost, product.selling_price, product.manufacturer.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    product.id = id
+    return id
